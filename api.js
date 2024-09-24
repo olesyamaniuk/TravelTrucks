171e4fe85c7753d8c5783d2ed4782f2490687c35
olesyamaniuk <maniukolesya222@gmail.com>
2024-09-24 15:01:14 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
+// export const getCars = async (page = 1, limit = 4) => {
+//   const url = `https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers?page=${page}&limit=${limit}`;
+//   const response = await axios.get(url);
+//   return response.data;
+// }
+
+axios.defaults.baseURL = 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/';
 export const getCars = async (page = 1, limit = 4) => {
-  const url = `https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers?page=${page}&limit=${limit}`;
-  const response = await axios.get(url);
-  return response.data;
-}
+  const response = await axios.get('campers', {
+    params: {
+      page: page,
+      limit: limit,
+    },
+  });
+  return response.data; // Залежно від формату відповіді API, може знадобитися уточнення
+};
 
 
 
